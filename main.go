dd6ba9ecd316f09026b6ddef95a1844149fb8d71
Ben Liu <ben.liu@tabcorp.com.au>
2018-02-07 10:15:25 +1100

    Add get person info from /people/:id

--- main.go
+++ main.go
@@ -29,11 +29,23 @@ func GetPeople(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
+// GetPerson to get one person data
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
 func main() {
 	router := mux.NewRouter()
 	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
 	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
 
 	log.Println("Server now listening on port", 8000)
 	log.Fatal(http.ListenAndServe(":8000", router))
